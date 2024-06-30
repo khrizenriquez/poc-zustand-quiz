43c28a276a90cd39e4fafe203565eae65f32e793
Christofer Enriquez <christoferen7@gmail.com>
2024-06-30 13:33:23 -0600

    Initial setup for the project, removed the components unused

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
